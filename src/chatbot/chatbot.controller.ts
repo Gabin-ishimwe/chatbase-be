eb17497dff45bd1a127f9abbce848fbdca7b86d3
gabin-ishimwe <g.ishimwe@alustudent.com>
2023-05-01 08:50:51 +0300

    ft: create chatbot logic

--- src/chatbot/chatbot.controller.ts
+++ src/chatbot/chatbot.controller.ts
@@ -1,10 +1,29 @@
 import { Controller, ParseFilePipe, Post, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ChatbotService } from './chatbot.service';
+import { CreateChatBot, FetchType } from './dto/create-chatbot.dto';
 
 @Controller('chatbot')
 export class ChatbotController {
+  constructor(private readonly chatbotService: ChatbotService) {}
+  @Post('create-bot')
+  @UseInterceptors(FileInterceptor('file'))
+  public async createChatBot(
+    @UploadedFile(new ParseFilePipe({ validators: [] }))
+    file: Express.Multer.File,
+    createChatBot: CreateChatBot,
+  ) {
+    switch (createChatBot.fetchSites) {
+      case FetchType.TEXT:
+        this.chatbotService.createChatBotFromText(createChatBot);
+        break;
+      case FetchType.FILE:
+        break;
+      case FetchType.WEBSITE:
+        break;
 
-    @Post('create-bot')
-    @UseInterceptors(FileInterceptor('file'))
-    public async createChatBot(@UploadedFile(new ParseFilePipe({validators: []})) file: Express.Multer.File)
+      default:
+        return null;
+    }
+  }
 }
--- src/chatbot/chatbot.service.ts
+++ src/chatbot/chatbot.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateChatBot } from './dto/create-chatbot.dto';
 
 @Injectable()
 export class ChatbotService {
   constructor(private prismaService: PrismaService) {}
 
-  public async createChatBot() {}
+  public async createChatBotFromText(createChatBot: CreateChatBot) {
+    const { name, description } = createChatBot;
+    const findChatbot = await this.prismaService.chatbot.findUnique({
+      where: {
+        name: createChatBot.name,
+      },
+    });
+    if (findChatbot)
+      return new BadRequestException({ message: 'Chatbot name already exist' });
+
+    /**
+     * openai api
+     */
+
+    const chatbot = await this.prismaService.chatbot.create({
+      data: {
+        name,
+        description,
+      },
+    });
+    return chatbot;
+  }
 }
